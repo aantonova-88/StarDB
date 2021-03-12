245fec523c30ea90150544889e3097b60340baac
Anna Antonova <anna88.ptz@gmail.com>
2021-03-12 09:37:16 +0300

    create a loading indicator

---
+++ src/components/spinner/spinner.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import './spinner.css';
+
+const Spinner = () => {
+  return (
+    <div class="loadingio-spinner-double-ring-a3712le0a5g">
+      <div class="ldio-ijmgst543a">
+    <div></div>
+    <div></div>
+    <div><div></div></div>
+    <div><div></div></div>
+    </div></div>
+  )
+}
+
+export default Spinner;
--- src/services/swapi-service.js
+++ src/services/swapi-service.js
@@ -44,7 +44,7 @@ export default class SwapiService {
 
   _extractId(item) {
     const idRegExp = /\/([0-9]*)\/$/;
-    item.url.match(idRegExp)[1];
+    return item.url.match(idRegExp)[1];
   }
 
   _transformPlanet(planet) {
